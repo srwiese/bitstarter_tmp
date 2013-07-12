2c9cb81f8d20eabc4c0a5131960005598d2a30e8
Scott Wiese <srwiese81@gmail.com>
2013-07-12 01:56:35 +0000

    added checks.json and grader.js

---
+++ web.js
@@ -0,0 +1,18 @@
+var express = require('express');
+
+var app = express.createServer(express.logger());
+
+var fs = require("fs");
+
+var responseBuff = fs.readFileSync('index.html');
+
+var responseString = responseBuff.toString('utf-8');
+
+app.get('/', function(request, response) {
+  response.send(responseString);
+});
+
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
